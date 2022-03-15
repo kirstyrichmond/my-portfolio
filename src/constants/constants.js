a78b2ca09938a9a0aee85b93bf0b7be96f8a23dc
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-03-15 17:46:43 +0000

    changed visit / source on projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,8 +5,8 @@ export const projects = [
       "Created the front-end architecture for be-nc-games. Northcoder Games is a social games web app which contains interactive ratings and discussions between users.",
     image: "/images/nc-games.png",
     tags: ["React", "HTML", "CSS"],
-    visit: "https://nc-games-kirsty-richmond.netlify.app/",
-    source: "https://github.com/kirstyrichmond/nc-games",
+    source: "https://nc-games-kirsty-richmond.netlify.app/",
+    visit: "https://github.com/kirstyrichmond/nc-games",
     id: 0,
   },
   {
@@ -15,8 +15,8 @@ export const projects = [
       "Created a games-based API to serve game categories, reviews, users and comment information to the front-end architecture.",
     image: "/images/be-nc-games.png",
     tags: ["Node", "Express", "PostgreSQL", "Jest"],
-    visit: "http://be-nc-games-app.herokuapp.com/api",
-    source: "https://github.com/kirstyrichmond/be-nc-games",
+    source: "http://be-nc-games-app.herokuapp.com/api",
+    visit: "https://github.com/kirstyrichmond/be-nc-games",
     id: 1,
   },
   {
@@ -26,7 +26,7 @@ export const projects = [
     image: "/images/readcycle.png",
     tags: ["Firebase", "React Native", "React Navigation", "Expo"],
     source: "https://github.com/kirstyrichmond/nc-readcycle",
-    // visit: "https://github.com/kirstyrichmond/nc-readcycle",
+    visit: "https://github.com/kirstyrichmond/nc-readcycle",
     id: 2,
   },
   {
@@ -35,8 +35,16 @@ export const projects = [
       "This web app allows the user to add, edit and delete tasks. These are kept in local storage. The user can click on a task if it has been completed. There is also a drag and drop functionality on each task. The user can filter the tasks to show all tasks, completed or uncompleted.",
     image: "/images/todo.png",
     tags: ["React", "Javascript", "HTML", "CSS"],
-    visit: "https://kirsty-richmond-todo-app.netlify.app",
-    source: "https://github.com/kirstyrichmond/To-do-Web-App",
+    source: "https://kirsty-richmond-todo-app.netlify.app",
+    visit: "https://github.com/kirstyrichmond/To-do-Web-App",
     id: 3,
   },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
