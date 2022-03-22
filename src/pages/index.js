8f8cc0ae8af2dd80a4557bedd599ef8369ef3f18
kirstyrichmond <kirsty.richmond.abc@hotmail.co.uk>
2022-03-22 15:53:22 +0000

    removed deleted import from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import Achievements from "../components/Acomplishments/Achievements";
-import BgAnimation from "../components/BackgrooundAnimation/BackgroundAnimation";
 import Hero from "../components/Hero/Hero";
 import Projects from "../components/Projects/Projects";
 import Technologies from "../components/Technologies/Technologies";
